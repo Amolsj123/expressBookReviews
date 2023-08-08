4d7ba38db360f7f384c9680c1eac6f77c77d3d4f
AMOL <130210618+Amolsj123@users.noreply.github.com>
2023-08-08 14:12:54 +0530

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,26 +5,60 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username)=>{ 
+  
+                // If password not entered
+                if (username == Amol)
+                    return true;
+                      
+                // If Not same return False.    
+                else if (username!= Amol) {
+                    return false;
+                }
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{ 
+  // If password not entered
+  if (username== Amol){
+    if (password== password){
+      return true;
+    }
+    else{
+      return false;
+    }       
+  }
+  return false;
 }
+
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
-
+ 
+    const username = req.body.username;
+    const password = req.body.password;
+  
+  
+    if (username && password) {
+      if (!doesExist(username)) { 
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User successfully registred. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});
+      }
+    } 
+    return res.status(404).json({message: "Unable to register user."});
+  });
+  
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  try {
+    const response = await axios.get(`bookssdb=${isbn}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
+
